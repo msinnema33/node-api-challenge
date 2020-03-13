d58fffc7d94f361655e1187ca53994bcfc863447
Mike Sinnema <msinnema33@gmail.com>
2020-03-13 08:18:32 -0700

    updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+require('dotenv').configure();
+
+const server = require('./api/server');
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+    console.log(`\n ***** Server is listening on port ${PORT} ***** \n`)
+});
+
+
+
+
+
 /*
 play this: https://www.youtube.com/watch?v=d-diB65scQU
 
